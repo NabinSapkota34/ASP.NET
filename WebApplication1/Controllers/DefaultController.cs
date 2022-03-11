53d3f9ae82da356284f9cc47ed832721a6e7c1d1
NabinSapkota34 <nawinssap123@gmail.com>
2022-03-11 08:43:32 +0545

    Table

--- WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/Controllers/DefaultController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class DefaultController : Controller
     {
+        empEntities db = new empEntities();
         // GET: Default
-        public ActionResult Index()
+        public ActionResult Services()
         {
+            List<employe> all_data = db.employes.ToList();
+            return View(all_data);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your page is";
             return View();
         }
     }
