fe96ad8b5399912c8df8020e131d23a2e99a3f42
NabinSapkota34 <nawinssap123@gmail.com>
2022-03-14 09:06:37 +0545

    Update Delete

--- WebApplication1/Controllers/nabinController.cs
+++ WebApplication1/Controllers/nabinController.cs
@@ -20,11 +20,38 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult Edit(int id)
+        {
+            employe data=db.employes.Find(id);
+            return View(data);
+        }
         public ActionResult SaveData(employe employe)
         {
             db.employes.Add(employe);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult UpdateData(employe employe)
+        {
+            employe update = db.employes.Find(employe.e_id);
+            update.e_name= employe.e_name;
+            update.salary= employe.salary;
+            db.Entry(update).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult delete(int id)
+        {
+            employe data = db.employes.Find(id);
+            return View(data);
+        }
+        public ActionResult DeleteData(employe employe)
+        {
+            employe delete = db.employes.Find(employe.e_id);
+            db.employes.Remove(delete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
